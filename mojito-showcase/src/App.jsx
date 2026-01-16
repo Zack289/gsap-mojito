966ac081a1bc1bfbd624473c0a1fae103d9f0b4c
Zack289 <ozack911@gmail.com>
2026-01-16 17:23:08 +0545

    Add smooth scroll with Lenis and update GSAP animations

--- mojito-showcase/src/App.jsx
+++ mojito-showcase/src/App.jsx
@@ -1,5 +1,9 @@
-import {ScrollTrigger, SplitText} from 'gsap/all'
-import { gsap } from 'gsap/gsap-core'
+import { useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Lenis from "@studio-freight/lenis";
+import { SplitText} from 'gsap/all'
+// import { gsap } from 'gsap/gsap-core'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
@@ -11,6 +15,25 @@ import Contact from './components/Contact';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
+
+   useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,       // smoothness
+      smoothWheel: true,   // smooth mouse wheel
+      smoothTouch: false,  // touchpad scroll stays natural
+      wheelMultiplier: 0.9
+    });
+
+    // Connect Lenis scroll to GSAP ScrollTrigger
+    lenis.on("scroll", ScrollTrigger.update);
+
+    // RAF loop for Lenis
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <main>
       <Navbar />
